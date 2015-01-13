193a44ed1abf5c2222034bebc209a34c1c9b9df5
yaojinyuan <yaojy@yaojydeMacBook-Pro.local>
2015-01-13 09:37:03 +0800

    Initial Commit

---
+++ 01AVAudioPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  01AVAudioPlayerDemo
+//
+//  Created by yaojinyuan on 15-1-13.
+//  Copyright (c) 2015年 福诺学院. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 01AVAudioPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  01AVAudioPlayerDemo
+//
+//  Created by yaojinyuan on 15-1-13.
+//  Copyright (c) 2015年 福诺学院. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
